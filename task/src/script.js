79da845fbf9f9a2b95a82c7c4d401a500a37fa9d
ekaterina <glebova.caterina2012@yandex.ru>
2021-10-06 11:17:08 +0300

    add TodoList

---
+++ task/src/script.js
@@ -0,0 +1,71 @@
+let d = document;
+let taskList = d.getElementById("task-list");
+let addTask = d.getElementById("add-task-button");
+let tasks = [];
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem("tasks"));
+addTasksInHTML();
+
+function  Task (name){
+    this.name= name;
+    this.check = false;
+}
+function pushLocaleStorage(){
+localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function addTasksInHTML(){
+    taskList.innerHTML = "";
+    if(tasks.length > 0) {
+        tasks.forEach((task, index) => {
+            createTask(task, index);
+        })
+    }
+}
+addTask.addEventListener("click",  function (e){
+    let taskName = d.getElementById("input-task").value;
+    tasks.push(new Task (taskName));
+    pushLocaleStorage();
+    d.getElementById("input-task").value = "";
+    addTasksInHTML();
+})
+function createTask(task, index) {
+    let li = d.createElement('li');
+    taskList.appendChild(li);
+    li.id = index;
+    let input = li.appendChild(d.createElement("input"));
+    input.setAttribute("type", "checkbox");
+    input.checked = task.check;    //////////////////////
+    input.id = index;
+    let span = li.appendChild( d.createElement("span"));
+    span.setAttribute("class", "task");
+    span.innerHTML = task.name;
+    let button = li.appendChild( d.createElement("button"));
+    button.id = index;
+    button.setAttribute("class",  "delete-btn");
+    button.innerHTML = "X";
+}
+
+taskList.addEventListener("click",  function (e){
+  let buttonClickItem = e.target;
+  let ind = buttonClickItem.id;
+    if(buttonClickItem.tagName  === "BUTTON") {
+        deleteTask(ind)
+    }
+    if (buttonClickItem.tagName === "INPUT"){
+         changeStatusCompleted(ind)
+    }
+})
+
+function deleteTask(ind) {
+    tasks.splice(ind,1);
+    d.getElementById(ind).remove();
+    pushLocaleStorage()
+    addTasksInHTML()
+}
+   function changeStatusCompleted(ind){
+    tasks[ind].check=!tasks[ind].check;
+    pushLocaleStorage()
+    addTasksInHTML()
+   }
+
+
+
